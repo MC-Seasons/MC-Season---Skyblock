02a1def8f5b1eb2067b59e9c5a5f6ad0226bfc08
MC-Seasons <53308965+MC-Seasons@users.noreply.github.com>
2019-08-10 20:22:57 +0200

    Fixed rojens mess

--- src/me/msc/skyblock/events/EntityDeath.java
+++ src/me/msc/skyblock/events/EntityDeath.java
@@ -1,10 +1,7 @@
 package me.msc.skyblock.events;
 
 import org.bukkit.Material;
-import org.bukkit.entity.Creeper;
-import org.bukkit.entity.Skeleton;
-import org.bukkit.entity.Spider;
-import org.bukkit.entity.Zombie;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -17,6 +14,8 @@ public class EntityDeath implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onEntityDeath(EntityDeathEvent e){
+
+        Random rand = new Random();
 
         if (e.getEntity() instanceof Skeleton){
             //Skeleton
@@ -33,9 +32,105 @@ public class EntityDeath implements Listener {
             e.getDrops().add(new ItemStack(Material.SULPHUR));
         }else if (e.getEntity() instanceof Spider){
             //Spider
-            Random rand = new Random();
             e.getDrops().clear();
             e.getDrops().add(new ItemStack(rand.nextInt(2)== 1 ? Material.SPIDER_EYE : Material.STRING));
+        }else if (e.getEntity() instanceof CaveSpider){
+            //Cave Spider
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(rand.nextInt(2)== 1 ? Material.SPIDER_EYE : Material.STRING));
+        }else if (e.getEntity() instanceof Enderman){
+            //Enderman
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(Material.ENDER_PEARL));
+        }else if (e.getEntity() instanceof Blaze){
+            //Blaze
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(Material.BLAZE_ROD));
+        }else if (e.getEntity() instanceof ElderGuardian){
+            //Elder Guardian
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(Material.PRISMARINE_SHARD));
+        }else if (e.getEntity() instanceof Endermite){
+            //Endermite
+            e.getDrops().clear();
+        }else if (e.getEntity() instanceof Evoker){
+            //Evoker
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(Material.TOTEM));
+            //Maybe Emerald??
+        }else if (e.getEntity() instanceof Ghast){
+            //Ghast
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(rand.nextInt(2) == 1 ? Material.GHAST_TEAR : Material.SULPHUR));
+        }else if (e.getEntity() instanceof Guardian){
+            //Guardian
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(Material.PRISMARINE_SHARD));
+        }else if (e.getEntity() instanceof Husk){
+            //Husk
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(Material.ROTTEN_FLESH));
+        }else if (e.getEntity() instanceof PigZombie){
+            //Zombie Pigman
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(rand.nextInt(2)==1 ? Material.ROTTEN_FLESH: Material.GOLD_NUGGET));
+        }else if (e.getEntity() instanceof Shulker){
+            //Shulker
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(rand.nextInt(2)==1 ? Material.SHULKER_SHELL: Material.AIR));
+        }else if (e.getEntity() instanceof Slime){
+            //Slime
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(Material.SLIME_BALL));
+        }else if (e.getEntity() instanceof PolarBear){
+            //Polar Bear
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(Material.RAW_FISH));
+        }else if (e.getEntity() instanceof Vex){
+            //Vex
+            e.getDrops().clear();
+        }else if (e.getEntity() instanceof Vindicator){
+            //Vindicator
+            e.getDrops().clear();
+            e.getDrops().add(new ItemStack(Material.EMERALD));
+        }else if (e.getEntity() instanceof Witch){
+            //Witch
+            e.getDrops().clear();
+            ItemStack itemStack = new ItemStack(Material.POTION,1,(short) 16);
+            e.getDrops().add(itemStack);
+        }else if (e.getEntity() instanceof Wither){
+            //Wither
+            e.getDrops().clear();
+            ItemStack itemStack = new ItemStack(Material.NETHER_STAR);
+            e.getDrops().add(itemStack);
+        }else if (e.getEntity() instanceof WitherSkeleton){
+            //Wither Skeleton
+            e.getDrops().clear();
+            ItemStack skull = new ItemStack(Material.SKULL, 1, (short) 1);
+            ItemStack coal = new ItemStack(Material.COAL);
+
+            e.getDrops().add(rand.nextInt(2)==1 ? skull : coal);
+        }else if (e.getEntity() instanceof ZombieHorse){
+            //Zombie Horse
+            e.getDrops().clear();
+            ItemStack itemStack = new ItemStack(Material.ROTTEN_FLESH);
+            e.getDrops().add(itemStack);
+        }else if (e.getEntity() instanceof ZombieVillager){
+            //Zombie Villager
+            e.getDrops().clear();
+            ItemStack itemStack = new ItemStack(Material.ROTTEN_FLESH);
+            e.getDrops().add(itemStack);
+        }else if (e.getEntity() instanceof MagmaCube){
+            //MagmaCube
+            e.getDrops().clear();
+            ItemStack itemStack = new ItemStack(Material.MAGMA_CREAM);
+            e.getDrops().add(itemStack);
+        }else if (e.getEntity() instanceof Silverfish){
+            //SilverFish
+            e.getDrops().clear();
+        }else if (e.getEntity() instanceof Wolf){
+            //Wolf
+            e.getDrops().clear();
         }
     }
 }
--- src/me/msc/skyblock/events/EntitySpawn.java
+++ src/me/msc/skyblock/events/EntitySpawn.java
@@ -53,6 +53,170 @@ public class EntitySpawn implements Listener {
             spider.setCustomName("Spider");
             spider.setCustomNameVisible(true);
 
+        }else if (e.getEntityType().equals(EntityType.CAVE_SPIDER)){
+            //Cave Spider
+            CaveSpider caveSpider = (CaveSpider) e.getEntity();
+
+            //name above the head
+            caveSpider.setCustomName("Cave Spider");
+            caveSpider.setCustomNameVisible(true);
+
+        }else if (e.getEntityType().equals(EntityType.ENDERMAN)){
+            //EnderMan
+            Enderman enderman = (Enderman) e.getEntity();
+
+            //name above the head
+            enderman.setCustomName("Enderman");
+            enderman.setCustomNameVisible(true);
+
+        }else if (e.getEntityType().equals(EntityType.BLAZE)){
+            //Blaze
+            Blaze blaze = (Blaze) e.getEntity();
+
+            //name above the head
+            blaze.setCustomName("Blaze");
+            blaze.setCustomNameVisible(true);
+
+        }else if (e.getEntityType().equals(EntityType.ELDER_GUARDIAN)){
+            //Elder guardian
+            ElderGuardian elderGuardian = (ElderGuardian) e.getEntity();
+
+            //name above the head
+            elderGuardian.setCustomName("Elder Guardian");
+            elderGuardian.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.ENDERMITE)){
+            //EnderMite
+            Endermite endermite = (Endermite) e.getEntity();
+
+            //name above the head
+            endermite.setCustomName("Endermite");
+            endermite.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.EVOKER)){
+            //Evoker
+            Evoker evoker = (Evoker) e.getEntity();
+
+            //name above the head
+            evoker.setCustomName("Evoker");
+            evoker.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.GHAST)){
+            //Ghast
+            Ghast ghast = (Ghast) e.getEntity();
+
+            //name above the head
+            ghast.setCustomName("Ghast");
+            ghast.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.GUARDIAN)){
+            //Guardian
+            Guardian guardian = (Guardian) e.getEntity();
+
+            //name above the head
+            guardian.setCustomName("Guardian");
+            guardian.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.HUSK)){
+            //Husk
+            Husk husk = (Husk) e.getEntity();
+
+            //name above the head
+            husk.setCustomName("Husk");
+            husk.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.PIG_ZOMBIE)){
+            //Zombie Pigman
+            PigZombie pigZombie = (PigZombie) e.getEntity();
+
+            //name above the head
+            pigZombie.setCustomName("Zombie Pigman");
+            pigZombie.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.SHULKER)){
+            //Shulker
+            Shulker shulker = (Shulker) e.getEntity();
+
+            //name above the head
+            shulker.setCustomName("Shulker");
+            shulker.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.SLIME)){
+            //Slime
+            Slime slime = (Slime) e.getEntity();
+
+            //name above the head
+            slime.setCustomName("Slime");
+            slime.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.POLAR_BEAR)){
+            //Polar Bear
+            PolarBear polarBear = (PolarBear) e.getEntity();
+
+            //name above the head
+            polarBear.setCustomName("Polar Bear");
+            polarBear.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.VEX)){
+            //Vex
+            Vex vex = (Vex) e.getEntity();
+
+            //name above the head
+            vex.setCustomName("Vex");
+            vex.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.VINDICATOR)){
+            //Vex
+            Vindicator vindicator = (Vindicator) e.getEntity();
+
+            //name above the head
+            vindicator.setCustomName("Vindicator");
+            vindicator.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.WITCH)){
+            //Witch
+            Witch witch = (Witch) e.getEntity();
+
+            //name above the head
+            witch.setCustomName("Witch");
+            witch.setCustomNameVisible(true);
+        }else if (e.getEntity().equals(EntityType.WITHER)){
+            //Wither
+            Wither wither = (Wither) e.getEntity();
+
+            //name above the head
+            wither.setCustomName("Wither");
+            wither.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.WITHER_SKELETON)){
+            //Wither Skeleton
+            WitherSkeleton witherSkeleton = (WitherSkeleton) e.getEntity();
+
+            //name about the head
+            witherSkeleton.setCustomName("Wither Skeleton");
+            witherSkeleton.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.ZOMBIE_HORSE)){
+            //Zombie Horse
+            ZombieHorse zombieHorse = (ZombieHorse) e.getEntity();
+
+            //name about the head
+            zombieHorse.setCustomName("Zombie Horse");
+            zombieHorse.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.ZOMBIE_VILLAGER)){
+            //Zombie Villager
+            ZombieVillager zombieVillager = (ZombieVillager) e.getEntity();
+
+            //name about the head
+            zombieVillager.setCustomName("Zombie Villager");
+            zombieVillager.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.MAGMA_CUBE)){
+            //Magma Cube
+            MagmaCube magmaCube = (MagmaCube) e.getEntity();
+
+            //name about the head
+            magmaCube.setCustomName("Magma Cube");
+            magmaCube.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.SILVERFISH)){
+            //Silver Fish
+            Silverfish silverfish = (Silverfish) e.getEntity();
+
+            //name about the head
+            silverfish.setCustomName("Silver Fish");
+            silverfish.setCustomNameVisible(true);
+        }else if (e.getEntityType().equals(EntityType.WOLF)){
+            //Wolf
+            Wolf wolf = (Wolf) e.getEntity();
+
+            //name about the head
+            wolf.setCustomName("Wolf");
+            wolf.setCustomNameVisible(true);
         }
     }
 
